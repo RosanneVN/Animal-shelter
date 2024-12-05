ca2b614a97c7b2a1e7e8d24d4e59eb9b070eb0de
RosanneVN <annevazquez05@gmail.com>
2024-12-04 18:41:10 -0600

    add colors in tailwind.config and fix it (pages)

--- app/tailwind.config.mjs
+++ app/tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 100%), url('/Image/BgProm.jpg')",
+      },
+      colors: {
+        primary: "#003049", 
+        secondary: "#c1121f",
+        terciary: "#669bbc",
+        lettersDark: "#0d0a0b",
+      lettersMiddle: "#999",
+        lettersLight: "#f3eff5"
+
+      },
+    },
+  },
+  plugins: [],
+};
