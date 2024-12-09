98f37ce718fb4cdb23e1fed35448622188d521ee
RosanneVN <annevazquez05@gmail.com>
2024-12-09 09:17:44 -0600

    Create Toggleable component

---
+++ app/src/components/ToggleableContent.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+type TypeProps = {
+  question: string;
+  answer: string;
+  isStartOpen?: boolean;
+};
+const ToggleableContent = ({
+  question,
+  answer,
+  isStartOpen = false,
+}: TypeProps) => {
+  // Estado para controlar la visibilidad del contenido
+  const [isOpen, setIsOpen] = useState(isStartOpen);
+
+  // Función para alternar la visibilidad
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex flex-col bg-lettersLight p-3 rounded-lg">
+      <div className="flex flex-row justify-between items-center">
+        <p className="text-lg text-secondary font-semibold">{question}</p>
+        <button
+          type="button"
+          onClick={toggle}
+          className="px-1 rounded-full bg-lettersMiddle w-fit h-fit text-lettersLight"
+        >
+          {isOpen ? "-" : "+"}
+        </button>
+      </div>
+
+      {/* Contenido desplegable con animación */}
+      <div
+        className={`transition-[max-height] duration-300 ease-in-out overflow-hidden ${
+          isOpen ? "max-h-40" : "max-h-0"
+        }`}
+      >
+        <div className="mt-2 p-2 border-t border-lettersMiddle">
+          <p className="text-base text-secondary">{answer}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleableContent;
