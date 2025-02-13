e707f73041f558a2be5e66feee0d7e0d75de4914
RosanneVN <annevazquez05@gmail.com>
2025-02-13 11:16:35 -0600

    fix perosnal Data form section

--- app/src/components/Forms/PersonalData.tsx
+++ app/src/components/Forms/PersonalData.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import InputForm from "../Inputs/InputForm";
+import BackAndNext from "../Buttons/BackAndNext";
 
 type FormData = {
   fullName: string;
@@ -43,85 +44,103 @@ const PersonalData = () => {
 
   return (
     <>
-      <form action="" className="py-9 px-10 flex flex-col gap-4">
-        <div className="flex flex-col gap-1">
-          <h3 className="text-center text-lg text-orange-400 font-semibold">
-            Formulario de adopción
-          </h3>
-          <h4 className="text-center text-sm font-semibold text-lettersDark">
-            Datos Personales
-          </h4>
-        </div>
+      <form
+        action=""
+        className="w-[60%] rounded-xl flex flex-col overflow-auto"
+      >
+        <div className="py-9 px-10 flex flex-col gap-4 h-full">
+          <div className="flex flex-col gap-1">
+            <h3 className="text-center text-lg text-orange-400 font-semibold">
+              Formulario de adopción
+            </h3>
+            <h4 className="text-center text-sm font-semibold text-lettersDark">
+              Datos Personales
+            </h4>
+          </div>
 
-        <div className="flex flex-col gap-5">
-          <InputForm
-            name={""}
-            label={"Nombre completo"}
-            type={"text"}
-            placeholderText={"Ej: Rosanne Vazquez"}
-            onChange={(inputValue) => {
-              handleChange("fullName", inputValue.target.value);
-            }}
-            isRequired={true}
-            value={values.fullName}
-            defaultValue={""}
-            errorMesage={!values.fullName? "Este campo es obligatorio" : undefined}
-          />
-          <InputForm
-            name={""}
-            label={"Edad"}
-            type={"number"}
-            placeholderText={"23"}
-            errorMesage={getEdadError()}
-            onChange={(inputValue) => {
-              handleChange("age", inputValue.target.value);
-            }}
-            isRequired={true}
-            value={values.age}
-            defaultValue={""}
-          />
-          <InputForm
-            name={""}
-            label={"Dirección de residencia (Municipio)"}
-            type={"text"}
-            placeholderText={"5ta A / 238 y 240 #23810, Playa, Jaimanistas"}
-            errorMesage={!values.address? "Este campo es obligatorio" : undefined}
-            onChange={(inputValue) => {
-              handleChange("address", inputValue.target.value);
-            }}
-            isRequired={true}
-            value={values.address}
-            defaultValue={""}
-          />
-          <InputForm
-            name={""}
-            label={"Teléfono de contacto principal"}
-            type={"tel"}
-            placeholderText={"56 77 52 45"}
-            errorMesage={!values.cellPhone? "Este campo es obligatorio" : undefined}
-            onChange={(inputValue) => {
-              handleChange("cellPhone", inputValue.target.value.trim());
-            }}
-            isRequired={true}
-            value={values.cellPhone}
-            defaultValue={""}
-          />
-          <InputForm
-            name={""}
-            label={"Teléfono alternativo (familiar o amigo cercano)"}
-            type={"tel"}
-            placeholderText={"56 77 52 45"}
-            errorMesage={!values.alternativeCellPhone? "Este campo es obligatorio" : undefined}
-            onChange={(inputValue) => {
-              handleChange(
-                "alternativeCellPhone",
-                inputValue.target.value.trim()
-              );
-            }}
-            isRequired={true}
-            value={values.alternativeCellPhone}
-            defaultValue={""}
-          />
+          <div className=" flex flex-col justify-between h-full gap-5">
+            <div className="flex flex-col gap-5">
+              <InputForm
+                name={""}
+                label={"Nombre completo"}
+                type={"text"}
+                placeholderText={"Ej: Rosanne Vazquez"}
+                onChange={(inputValue) => {
+                  handleChange("fullName", inputValue.target.value);
+                }}
+                isRequired={true}
+                value={values.fullName}
+                defaultValue={""}
+                errorMesage={
+                  !values.fullName ? "Este campo es obligatorio" : undefined
+                }
+              />
+              <InputForm
+                name={""}
+                label={"Edad"}
+                type={"number"}
+                placeholderText={"23"}
+                errorMesage={getEdadError()}
+                onChange={(inputValue) => {
+                  handleChange("age", inputValue.target.value);
+                }}
+                isRequired={true}
+                value={values.age}
+                defaultValue={""}
+              />
+              <InputForm
+                name={""}
+                label={"Dirección de residencia (Municipio)"}
+                type={"text"}
+                placeholderText={"5ta A / 238 y 240 #23810, Playa, Jaimanistas"}
+                errorMesage={
+                  !values.address ? "Este campo es obligatorio" : undefined
+                }
+                onChange={(inputValue) => {
+                  handleChange("address", inputValue.target.value);
+                }}
+                isRequired={true}
+                value={values.address}
+                defaultValue={""}
+              />
+              <InputForm
+                name={""}
+                label={"Teléfono de contacto principal"}
+                type={"tel"}
+                placeholderText={"56 77 52 45"}
+                errorMesage={
+                  !values.cellPhone ? "Este campo es obligatorio" : undefined
+                }
+                onChange={(inputValue) => {
+                  handleChange("cellPhone", inputValue.target.value.trim());
+                }}
+                isRequired={true}
+                value={values.cellPhone}
+                defaultValue={""}
+              />
+              <InputForm
+                name={""}
+                label={"Teléfono alternativo (familiar o amigo cercano)"}
+                type={"tel"}
+                placeholderText={"56 77 52 45"}
+                errorMesage={
+                  !values.alternativeCellPhone
+                    ? "Este campo es obligatorio"
+                    : undefined
+                }
+                onChange={(inputValue) => {
+                  handleChange(
+                    "alternativeCellPhone",
+                    inputValue.target.value.trim()
+                  );
+                }}
+                isRequired={true}
+                value={values.alternativeCellPhone}
+                defaultValue={""}
+              />
+            </div>
+            <BackAndNext></BackAndNext>
+          </div>
         </div>
       </form>
     </>
