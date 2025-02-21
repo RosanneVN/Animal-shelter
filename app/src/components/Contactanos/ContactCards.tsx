28ca468fc62fa4e3893fc4caba6b043039d6a20e
RosanneVN <annevazquez05@gmail.com>
2025-02-20 22:31:48 -0600

    featWip: update Visitanos section

--- app/src/components/Contactanos/ContactCards.tsx
+++ app/src/components/Contactanos/ContactCards.tsx
@@ -1,7 +1,16 @@
-const ContactCards = () => {
+type Props = {
+  nameContact: string;
+  numberContact: any;
+};
+
+const ContactCards = ({ nameContact, numberContact }: Props) => {
   return (
     <>
-      <div></div>
+      <div className="flex flex-row shadow-lg my-10 rounded-md bg-white w-96 h-20 px-5
+        justify-between items-center text-lettersDark hover:translate-y-1">
+        <p>{nameContact}</p>
+        <p>{numberContact}</p>
+      </div>
     </>
   );
 };
