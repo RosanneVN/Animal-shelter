24d5305ca422f6c687226dff56ade7a65b0494d4
erickdevup <erickdevup@gmail.com>
2025-02-22 08:57:45 +0100

    fix card modal

--- app/src/components/CreditCardsCoontent.tsx
+++ app/src/components/CreditCardsCoontent.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { CreditCardsProvider } from "../Context/CreditCardsContext";
 import CreditCardsList from "./ApadrinaYDona/CreditCardsList";
-import ModalForm from "./ModalForm";
-
+import ModalForm from "../layouts/ModalForm";
+import { ModalFormProvider } from "../Context/ModalFormContext";
 type Props = {};
 
 const CreditCardsCoontent = (props: Props) => {
   return (
     <>
-       <section className=" w-full">
-        <CreditCardsProvider>
-          <ModalForm></ModalForm>
+      <section className=" w-full">
+        <ModalFormProvider>
+          <ModalForm>Holaaa</ModalForm>
           <CreditCardsList />
-        </CreditCardsProvider>
+        </ModalFormProvider>
       </section>
     </>
   );
