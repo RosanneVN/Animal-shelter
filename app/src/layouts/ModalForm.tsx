b9564f1f3c925c14fb1efae1dc8f81d4ba326d66
RosanneVN <annevazquez05@gmail.com>
2025-03-25 20:36:27 -0600

    Ft: fix modal form

--- app/src/layouts/ModalForm.tsx
+++ app/src/layouts/ModalForm.tsx
@@ -7,16 +7,18 @@ type Props = {
 
 const ModalForm = ({ children }: Props) => {
   const { isOpen, setIsOpen } = useContext(ModalFormContext);
-  
-
+  const handleBackdropClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) {
+      setIsOpen(false);
+    }
+  };
   return isOpen ? (
     <section
-      onClick={() => {
-        setIsOpen(false);
-      }}
-      className="backdrop-blur-md bg-black/30 w-screen h-[100vh] fixed bottom-0 left-0 z-50 
+      onClick={handleBackdropClick}
+      className="backdrop-blur-md bg-black/30 w-screen h-[100vh] fixed bottom-0 left-0 z-40 
     place-content-center place-items-center"
     >
+      <div></div>
       {children}
     </section>
   ) : null;
