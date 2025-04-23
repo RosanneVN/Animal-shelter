2cd3c7c9826d3b1ff1efc45b2b557facb5225436
RosanneVN <annevazquez05@gmail.com>
2025-04-23 09:27:12 -0600

    feat: create Adoption requests db table

--- app/db/config.ts
+++ app/db/config.ts
@@ -1,16 +1,68 @@
-import { defineDb, defineTable, column } from 'astro:db';
+import { defineDb, defineTable, column } from "astro:db";
 
 const Pets = defineTable({
   columns: {
-   id: column.text({ primaryKey: true }),
-   petname: column.text(),
-   age: column.number(),
-   gender: column.text(),
-   species: column.text()
+    id: column.text({ primaryKey: true }),
+    petname: column.text(),
+    age: column.number(),
+    gender: column.text(),
+    species: column.text(),
+  },
+});
 
-  }
-})
+const AdoptionRequestsDB = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    //personal data
+    fullname: column.text(),
+    age: column.number(),
+    cellPhone: column.number(),
+    alternativeCellPhone: column.number(),
+    address: column.text(),
+
+    //motivaciones
+
+    motivation: column.text(),
+    forWho: column.text(),
+    petMoney: column.text(),
+    petFollowing: column.text(),
+    notAbandoned: column.text(),
+
+    //Home conditions
+    ownHouse: column.text(),
+    agreeRent: column.text(),
+    bigPlace: column.text(),
+    sleepPlace: column.text(),
+    houseNotScape: column.text(),
+    childrens: column.text(),
+    petAlergic: column.text(),
+
+    //Experience
+    family: column.text(),
+    adoptionAgree: column.text(),
+    howManyPets: column.number(),
+    petsBefore: column.text(),
+    petsBeforeAlive: column.text(),
+
+    //LifeStyle
+    job: column.text(),
+    iftravel: column.text(),
+    petIfTravel: column.text(),
+    otherHouse: column.text(),
+
+    //Knowledge
+    petDoctorClose: column.text(),
+    vacunationSchema: column.text(),
+    sterilizationOpinion: column.text(),
+
+    //Documentation
+    youAgree: column.text(),
+
+    //union de tablas, llave foranea
+    petId: column.text({ references: () => Pets.columns.id }),
+  },
+});
 
 export default defineDb({
-  tables: { Pets },
-})
+  tables: { Pets, AdoptionRequestsDB },
+});
