f67f67c22648dcefa48eaab8ae34afa5aea6e24c
RosanneVN <annevazquez05@gmail.com>
2025-04-23 10:11:31 -0600

    feat: create POST methos in api

--- app/src/pages/api/adoptionRequest.ts
+++ app/src/pages/api/adoptionRequest.ts
@@ -1,5 +1,10 @@
 import type { APIRoute } from "astro";
 import { AdoptionRequestsDB, db } from "astro:db";
+import { v4 as uuidv4 } from "uuid";
+import {
+  AdoptionReqSchema,
+  type CreateAdoptionReqInput,
+} from "../../Backend/Schemas/AdoptionReq.schemas";
 
 export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
@@ -13,4 +18,44 @@ export const GET: APIRoute = async ({ request }) => {
       "Content-Type": "application/json",
     },
   });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const body = await request.json();
+    const validationResults = AdoptionReqSchema.safeParse(body);
+    if (!validationResults.success) {
+      return new Response(
+        JSON.stringify({
+          error: validationResults.error.flatten().fieldErrors,
+        }),
+        {
+          status: 400,
+          statusText: "Michi not found",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    const validationData: CreateAdoptionReqInput = validationResults.data;
+    const adoptionReq = await db
+      .insert(AdoptionRequestsDB)
+      .values({ id: uuidv4(), ...validationData });
+
+    return new Response(JSON.stringify(adoptionReq), {
+      status: 200,
+      statusText: "Michi created",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Error en el servidor" }), {
+      status: 500,
+      statusText: "Error en el servidor",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 };
