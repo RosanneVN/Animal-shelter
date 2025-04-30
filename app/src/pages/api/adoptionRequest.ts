1db23dbb3d08a530d2b2d9f191fa56d0c9d921f5
RosanneVN <annevazquez05@gmail.com>
2025-04-29 21:05:05 -0600

    feat: create GET and POST HTTP method (Adoption requests)

--- app/src/pages/api/adoptionRequest.ts
+++ app/src/pages/api/adoptionRequest.ts
@@ -1,15 +1,15 @@
 import type { APIRoute } from "astro";
-import { AdoptionRequestsDB, db } from "astro:db";
+import { AdoptionRequestsDB, db, eq } from "astro:db";
 import { v4 as uuidv4 } from "uuid";
 import {
   AdoptionReqSchema,
+  deleteAdoptionReqSchema,
   type CreateAdoptionReqInput,
 } from "../../Backend/Schemas/AdoptionReq.schemas";
 
 export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
   const adoptionReq = await db.select().from(AdoptionRequestsDB);
-  console.log("adoptionReq", adoptionReq);
 
   return new Response(JSON.stringify({ data: adoptionReq }), {
     status: 200,
@@ -23,7 +23,17 @@ export const GET: APIRoute = async ({ request }) => {
 export const POST: APIRoute = async ({ request }) => {
   try {
     const body = await request.json();
+    console.log("body", body);
     const validationResults = AdoptionReqSchema.safeParse(body);
+    console.log("validationResults", validationResults);
+    console.log(
+      "validationResults.error",
+      JSON.stringify({
+        error: validationResults?.error?.flatten().fieldErrors,
+      })
+    );
+    console.log("validationResults.success", validationResults.success);
+
     if (!validationResults.success) {
       return new Response(
         JSON.stringify({
@@ -41,7 +51,7 @@ export const POST: APIRoute = async ({ request }) => {
     const adoptionReq = await db
       .insert(AdoptionRequestsDB)
       .values({ id: uuidv4(), ...validationData });
-    console.log(adoptionReq);
+    console.log("adoptionReq", adoptionReq);
 
     return new Response(JSON.stringify(adoptionReq), {
       status: 200,
@@ -58,5 +68,44 @@ export const POST: APIRoute = async ({ request }) => {
         "Content-Type": "application/json",
       },
     });
+  }
+};
+
+export const DELETE: APIRoute = async ({ request }) => {
+  try {
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    if (!id) {
+      throw new Error("ID not found");
+    }
+    const validationResult = deleteAdoptionReqSchema.safeParse({ id });
+    if (!validationResult.success) {
+      return new Response(
+        JSON.stringify({
+          errors: validationResult.error.flatten().fieldErrors,
+        }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+    const adoptionReq = await db
+      .delete(AdoptionRequestsDB)
+      .where(eq(AdoptionRequestsDB.id, id));
+    return new Response(
+      JSON.stringify({ message: `Michi con id ${id} eliminado` }),
+      {
+        status: 200,
+        statusText: "Michi eliminado",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Error al procesar la solicitud" }),
+      {
+        status: 500,
+        statusText: "Error en el servidor",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 };
