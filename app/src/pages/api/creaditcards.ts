1376ee565a89c7763843a894b2e6bf539535757d
RosanneVN <annevazquez05@gmail.com>
2025-05-06 09:25:42 -0600

    feat: create CreditCards http methods

---
+++ app/src/pages/api/creaditcards.ts
@@ -0,0 +1,135 @@
+import type { APIRoute } from "astro";
+import { CreditCardsDB, db, eq } from "astro:db";
+import {
+  CreditCardsSchema,
+  type CreateCreditCardsInput,
+} from "../../Backend/Schemas/CreditCards.schema";
+
+export const GET: APIRoute = async ({}) => {
+  const creditCards = await db.select().from(CreditCardsDB);
+  return new Response(JSON.stringify({ data: creditCards }), {
+    status: 200,
+    statusText: "Michi encontrado",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const body = await request.json();
+    const validationResults = CreditCardsSchema.safeParse(body);
+    if (!validationResults.success) {
+      return new Response(
+        JSON.stringify({
+          error: validationResults.error.flatten().fieldErrors,
+        }),
+        {
+          status: 400,
+          statusText: "Michi not found",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+    const validationData: CreateCreditCardsInput = validationResults.data;
+    const creditCards = await db.insert(CreditCardsDB).values(validationData);
+    return new Response(JSON.stringify(creditCards), {
+      status: 200,
+      statusText: "Michi created",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Error en el servidor" }), {
+      status: 500,
+      statusText: "Error en el servidor",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+};
+
+export const DELETE: APIRoute = async ({ request }) => {
+  try {
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    if (!id) {
+      throw new Error("ID not found");
+    }
+
+    const validationResult = CreditCardsSchema.safeParse({ id });
+    if (!validationResult.success) {
+      return new Response(
+        JSON.stringify({
+          errors: validationResult.error.flatten().fieldErrors,
+        }),
+        { status: 400, headers: { "Content-Type": "application/json" } }
+      );
+    }
+
+    const creditCards = await db
+      .delete(CreditCardsDB)
+      .where(eq(CreditCardsDB.id, id));
+    return new Response(
+      JSON.stringify({ message: `Michi con id ${id} eliminado` }),
+      {
+        status: 200,
+        statusText: "Michi eliminado",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Error al procesar la solicitud" }),
+      {
+        status: 500,
+        statusText: "Error en el servidor",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
+
+export const PATCH: APIRoute = async ({ request }) => {
+  try {
+    const body = await request.json();
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    if (!id) {
+      throw new Error("ID not found");
+    }
+    const dataValidadtion = CreditCardsSchema.safeParse(body);
+    if (!dataValidadtion.success) {
+      return new Response(
+        JSON.stringify({ error: dataValidadtion.error.flatten().fieldErrors }),
+        {
+          status: 400,
+          statusText: "Michi not found",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+    const validatedData: CreateCreditCardsInput = dataValidadtion.data;
+    const updateData = await db
+      .update(CreditCardsDB)
+      .set(validatedData)
+      .where(eq(CreditCardsDB.id, id));
+    return new Response(JSON.stringify(updateData), {
+      status: 200,
+      statusText: "Michi actualizado",
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Error al procesar la solicitud" }),
+      {
+        status: 500,
+        statusText: "Error en el servidor",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
