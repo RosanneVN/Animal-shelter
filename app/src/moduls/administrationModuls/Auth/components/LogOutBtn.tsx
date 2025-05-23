533f93d6cb6e07c62f00e07f45e1dde58767c589
RosanneVN <annevazquez05@gmail.com>
2025-05-22 20:35:09 -0600

    feat: add logout SVG icon and update LogoutButton component

--- app/src/moduls/administrationModuls/Auth/components/LogOutBtn.tsx
+++ app/src/moduls/administrationModuls/Auth/components/LogOutBtn.tsx
@@ -1,30 +1,36 @@
 export default function LogoutButton() {
   const handleLogout = async () => {
     try {
-      const response = await fetch('/api/auth/logout', {
-        method: 'POST',
+      const response = await fetch("/api/auth/logout", {
+        method: "POST",
       });
 
       if (response.ok) {
         // Logout exitoso, redirigir al login
-        window.location.href = '/login'; // O a la página que desees
+        window.location.href = "/login"; // O a la página que desees
       } else {
         // Manejar error en el logout
-        console.error('Logout failed:', await response.text());
+        console.error("Logout failed:", await response.text());
         // Podrías mostrar un mensaje al usuario
       }
     } catch (error) {
-      console.error('An error occurred during logout:', error);
+      console.error("An error occurred during logout:", error);
       // Manejar errores de red u otros
     }
   };
 
   return (
-    <button
-      onClick={handleLogout}
-      className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-    >
-      Cerrar Sesión
-    </button>
+    <div className="pl-20">
+      <button
+        onClick={handleLogout}
+        className="flex gap-2 text-[#ff3b30] font-semibold text-shortLetters  "
+      >
+        Cerrar Sesión{" "}
+        <span>
+          {" "}
+          <img className="size-5" src="/Icons/SVGs/logout.svg" alt="" />
+        </span>
+      </button>
+    </div>
   );
 }
