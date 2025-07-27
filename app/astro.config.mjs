4ea2e07272951b8407328faeadfaf5375d751950
erickdevup <erickdevup@gmail.com>
2025-07-27 07:26:33 +0200

    chore: format adapter line in astro.config.mjs for consistency

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -6,13 +6,13 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import db from "@astrojs/db";
-import cloudflare from "@astrojs/cloudflare";
+//import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), db()],
   output: "server",
-  adapter: cloudflare(),
+ //adapter: cloudflare(),
   vite: {
     ssr: {
       noExternal: ["@uiw/react-md-editor", "@uiw/react-markdown-preview"],
