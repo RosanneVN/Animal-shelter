fd99e5493ea146321ba64d36e78b37adbd158cf5
erickdevup <erickdevup@gmail.com>
2025-07-27 08:26:06 +0200

    Refactor code structure for improved readability and maintainability

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -6,16 +6,20 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import db from "@astrojs/db";
+import vercel from "@astrojs/vercel";
 //import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), db()],
   output: "server",
+
   //adapter: cloudflare(),
   vite: {
     ssr: {
       noExternal: ["@uiw/react-md-editor", "@uiw/react-markdown-preview"],
     },
   },
+
+  adapter: vercel(),
 });
